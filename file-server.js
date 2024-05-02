c37ee1ed559be9b2ed99addbdc8f38a05b69cdeb
BaeMinGyu <baemingyu5@gmail.com>
2024-05-03 00:15:41 +0900

    good

---
+++ file-server.js
@@ -0,0 +1,20 @@
+const http = require("http"); 
+const fs = require("fs");
+const server = http.createServer( (req, resp) => { 
+    const filename = "public/venice.jpg"; 
+  
+    try { 
+        const file = fs.readFileSync(filename); 
+        resp.writeHead(200, {"Content-Type": "image/jpeg"}); 
+        resp.write(file); 
+        resp.end(); 
+        } catch { 
+        resp.writeHead(500, {"Content-Type": "text/html"}); 
+        resp.write( 
+        "<h1>500 Error - File not found</h1>\n"); 
+        resp.end(); 
+        }
+   }); 
+   let port = 8080; 
+   server.listen(port); 
+   console.log("Server running at port= " + port);
