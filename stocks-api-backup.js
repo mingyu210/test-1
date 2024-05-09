2fe7e8378960ff89fc3b5e7ef64c764188d90a5c
Baemingyu <baemingyu5@gmail.com>
2024-05-09 10:13:09 +0900

    good

---
+++ stocks-api-backup.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+
+
+const provider = require('./scripts/data-provider.js');
+const stocks = provider.data;
+
+const app = express();
+
+app.use('/static',
+express.static(path.join(__dirname,'public')));
+
+
+// define the API routes
+// return all the stocks when a root request arrives
+app.get('/', (req,resp) => { resp.json(stocks) } );
+// Use express to listen to port
+// return just the requested stock
+app.get('/stock/:symbol', (req,resp) => {
+    // change user supplied symbol to upper case
+    const symbolToFind = req.params.symbol.toUpperCase();
+    // search the array of objects for a match
+    const matches =
+    stocks.filter(obj => symbolToFind === obj.symbol);
+    // return the matching stock
+    resp.json(matches);
+    });
+
+    // return all the stocks whose name contains the supplied text
+app.get('/stock/name/:substring', (req,resp) => {
+    // change user supplied substring to lower case
+    const substring = req.params.substring.toLowerCase();
+    // search the array of objects for a match
+    const matches = stocks.filter( (obj) =>
+    obj.name.toLowerCase().includes(substring) );
+    // return the matching stocks
+    resp.json(matches);
+    });
+
+
+let port = 8080;
+app.listen(port, () => {
+console.log("Server running at port= " + port);
+});
